a2162c5420e8fa0455ff8461ce776e67c733e7c8
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2023-08-21 16:24:56 -0300

    Create index.ino

---
+++ index.ino
@@ -0,0 +1,14 @@
+#include <WiFi.h>
+ 
+void setup(){
+    Serial.begin(115200);
+    Serial.print("\nDefault ESP32 MAC Address: ");
+    Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+  
+
+
+
+}
